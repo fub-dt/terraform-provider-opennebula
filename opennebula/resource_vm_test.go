c30652e8315f4d214ecb13b9551d843ffcbf5f3c
Bernhard Fuchshumer <bernhard.fuchshumer@dynatrace.com>
2018-07-25 14:54:37 +0200

    add acceptance test for VM resource
    -add rm resource acceptance test similar to the already existing
    acceptance tests for other resources.
    -in addition to setting the endpoint, username, and password env vars, at
    least the vm's template id has to be provided as env var
    ('OPENNEBULA_VM_TEMPLATE_ID'). Optionally, OPENNEBULA_VM_WAIT_FOR_ATTRIBUTE
    as well as OPENNEBULA_VM_IP_ATTRIBUTE can be set. These settings highly
    depend on the environment the acceptance test is run in, therefore they
    need to be adapted to fit the environment.

--- opennebula/resource_vm_test.go
+++ opennebula/resource_vm_test.go
@@ -1,9 +1,15 @@
 package opennebula
 
 import (
+	"encoding/xml"
 	"fmt"
+	"os"
+	"reflect"
+	"strings"
 	"testing"
 
+	"github.com/hashicorp/terraform/helper/resource"
+	"github.com/hashicorp/terraform/terraform"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
 )
@@ -132,4 +138,137 @@ func TestParsingVmInfoConcatCharInfo(t *testing.T) {
 	assert.Len(t, attributes, 2)
 	assert.Equal(t, "child value", attributes["PARENT_ELEMENT/CHILD_ELEMENT"])
 	assert.Equal(t, "parent value", attributes["PARENT_ELEMENT"])
+}
+
+var vmConfigBasicTemplate = `
+resource "opennebula_vm" "test" {
+  name = "test-vm"
+  permissions = "642"
+  %s
+}
+`
+
+var vmConfigUpdateTemplate = `
+resource "opennebula_vm" "test" {
+  name = "test-vm"
+  permissions = "666"
+  %s
+}
+`
+
+func TestAccVm(t *testing.T) {
+	baseConfig := createConfig(vmConfigBasicTemplate)
+	updateConfig := createConfig(vmConfigUpdateTemplate)
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckVmDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: baseConfig,
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("opennebula_vm.test", "name", "test-vm"),
+					resource.TestCheckResourceAttr("opennebula_vm.test", "template_id", getTemplateId()),
+					resource.TestCheckResourceAttr("opennebula_vm.test", "wait_for_attribute", getWaitForAttribute()),
+					resource.TestCheckResourceAttr("opennebula_vm.test", "ip_attribute", getIpAttribute()),
+					resource.TestCheckResourceAttr("opennebula_vm.test", "permissions", "642"),
+					resource.TestCheckResourceAttrSet("opennebula_vm.test", "ip"),
+					resource.TestCheckResourceAttrSet("opennebula_vm.test", "uid"),
+					resource.TestCheckResourceAttrSet("opennebula_vm.test", "gid"),
+					resource.TestCheckResourceAttrSet("opennebula_vm.test", "uname"),
+					resource.TestCheckResourceAttrSet("opennebula_vm.test", "gname"),
+					testAccCheckVmPermissions(&Permissions{
+						Owner_U: 1,
+						Owner_M: 1,
+						Group_U: 1,
+						Other_M: 1,
+					}),
+				),
+			},
+			{
+				Config: updateConfig,
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("opennebula_vm.test", "permissions", "666"),
+					testAccCheckVmPermissions(&Permissions{
+						Owner_U: 1,
+						Owner_M: 1,
+						Group_U: 1,
+						Group_M: 1,
+						Other_U: 1,
+						Other_M: 1,
+					}),
+				),
+			},
+		},
+	})
+}
+
+func createConfig(template string) string {
+	additionalConfigTemplate := "template_id = %s\n  wait_for_attribute=\"%s\"\n  ip_attribute=\"%s\""
+	additionalConfig := fmt.Sprintf(additionalConfigTemplate, getTemplateId(), getWaitForAttribute(), getIpAttribute())
+
+	return fmt.Sprintf(template, additionalConfig)
+}
+
+func getTemplateId() string {
+	return readEnvironmentVariable("OPENNEBULA_VM_TEMPLATE_ID", "")
+}
+
+func getWaitForAttribute() string {
+	return readEnvironmentVariable("OPENNEBULA_VM_WAIT_FOR_ATTRIBUTE", DefaultIpAttribute)
+}
+
+func getIpAttribute() string {
+	return readEnvironmentVariable("OPENNEBULA_VM_IP_ATTRIBUTE", DefaultIpAttribute)
+}
+
+func readEnvironmentVariable(key string, defaultValue string) string {
+	value := os.Getenv(key)
+	if value != "" {
+		return value
+	} else {
+		return defaultValue
+	}
+}
+
+func testAccCheckVmDestroy(s *terraform.State) error {
+	client := testAccProvider.Meta().(*Client)
+
+	for _, rs := range s.RootModule().Resources {
+		resp, err := client.Call("one.vm.info", intId(rs.Primary.ID), false)
+		if err == nil && !strings.Contains(resp, "<STATE>6</STATE>") {
+			return fmt.Errorf("Expected vm %s to have been destroyed", rs.Primary.ID)
+		}
+	}
+
+	return nil
+}
+
+func testAccCheckVmPermissions(expected *Permissions) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		client := testAccProvider.Meta().(*Client)
+
+		for _, rs := range s.RootModule().Resources {
+			resp, err := client.Call("one.vm.info", intId(rs.Primary.ID), false)
+			if err != nil {
+				return fmt.Errorf("Expected vm %s to exist when checking permissions", rs.Primary.ID)
+			}
+
+			var vm UserVm
+			if err = xml.Unmarshal([]byte(resp), &vm); err != nil {
+				return err
+			}
+
+			if !reflect.DeepEqual(vm.Permissions, expected) {
+				return fmt.Errorf(
+					"Permissions for vnet %s were expected to be %s. Instead, they were %s",
+					rs.Primary.ID,
+					permissionString(expected),
+					permissionString(vm.Permissions),
+				)
+			}
+		}
+
+		return nil
+	}
 }
